24b1b8473e7e9677d2d3759aa99d1cbca6bfc6c7
jayparmar271 <jayparmar271@gmail.com>
2022-07-30 10:46:36 +0530

    Update post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,6 +23,7 @@ class PostController extends Controller
         $post = Post::find($post->id);
         $post->title = $request->title;
         $post->description = $request->description;
+        $post->category_id = $request->category_id;
         $post->save();
 
         return response()->json(['post' => $post]);
