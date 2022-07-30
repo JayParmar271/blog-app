eca97612b1a2c2611cc07ab587bcd10e931d3fba
jayparmar271 <jayparmar271@gmail.com>
2022-07-30 08:23:03 +0530

    Add: Login and Registration in API

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => \Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('Token')->accessToken;
+
+        return response()->json(['token' => $token, 'user' => $user], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('Token')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+     }
+}
